6dde6bb35ca1917b5afd3e7e57a4581d0cd7f975
Knkiss4090 <962443828@qq.com>
2023-11-27 12:31:53 +0800

    run.sh

--- code/world.py
+++ code/world.py
@@ -89,7 +89,7 @@ device = torch.device('cuda' if GPU else "cpu")
 # region 功能设置
 PATH_ROOT = "D:/Codes/rec_torch"
 if platform.system().lower() == 'linux':
-    PATH_ROOT = "/home/byl/PycharmProjects/rec_torch"
+    PATH_ROOT = "/home/byl/code/rec_torch"
 PATH_CODE = join(PATH_ROOT, 'code')
 PATH_DATA = join(PATH_ROOT, 'data')
 PATH_OUTPUT = join(PATH_ROOT, 'output')
---
+++ run.sh
@@ -0,0 +1,4 @@
+cd code || exit
+echo "开始运行rec torch脚本"
+
+python main.py
